72bdb57e2bc39c6c58c3f47d08323bf4083c27fe
James <jamesdsallen@live.co.uk>
2019-08-22 16:53:06 +0100

    MatrixMinMaxProblem

---
+++ C#/MazeProblem/MinMaxMazeProblem/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 1) Provided with any size MxN matrix.
+/// 2) Find all the routes from top left square to bottom right:
+///     ---------
+///     | 8 | 3 | 
+///     ---------   Potential routes (8-5-4 or 8-3-4)
+///     | 5 | 4 | 
+///     ---------
+/// 3) Of those routes found, find the minimum value on each route. 
+///    For the example above this would be: 4 and 3 respectively.
+/// 4) Out of these minimum values find the maximum.
+///    For the example above this would be: 4 (as 4 > 3).
+///    
+/// TODO: implement dynamic programming (memoization)
+/// </summary>
+
+
+namespace MinMaxMazeProblem
+{
+    public class Program
+    {
+        public static int displayPath(List<int> path, int last, int max)
+        {
+            int minimum = path[0];
+            path.Add(last);
+            foreach (int step in path)
+            {
+                if (step < minimum)
+                    minimum = step;
+
+                Console.Write(step + "\t");
+            }
+            Console.WriteLine();
+
+            if (minimum > max)
+                max = minimum;
+
+            path.RemoveAt(path.Count - 1);
+            return max;
+        }
+
+        //Recursive function
+        public static int possiblePaths(int x, int y, int[,] m, int rowCount, int colCount, int max, List<int> path)
+        {
+            if (x == (rowCount - 1) && y == (colCount - 1))
+            {
+                max = displayPath(path, m[x,y], max);
+                return max;
+            }
+
+            path.Add(m[x, y]);
+
+            if ((x >= 0 && x < rowCount  && y + 1 >= 0 && y + 1 < colCount))
+            {
+                max = possiblePaths(x,y+1,m,rowCount,colCount,max,path);
+            }
+
+            if ((x + 1 >= 0 && x + 1 < rowCount && y >= 0 && y < colCount))
+            {
+                max = possiblePaths(x+1,y, m, rowCount, colCount,max,path);
+            }
+
+            path.RemoveAt(path.Count() - 1);
+            return max;
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> path = new List<int>();
+            int currentMaximum = 0;
+            int x = 0, y = 0;
+            int rowCount = 0, colCount = 0;
+            int[,] m = {   { 7, 11, 1 }, 
+                           { 9, 2, 12 }, 
+                           { 1, 43, 10 }};
+
+            rowCount = m.GetLength(0);
+            colCount = m.GetLength(1);
+            currentMaximum = possiblePaths(x, y, m, rowCount, colCount, currentMaximum, path);
+            Console.WriteLine("Maximum of the minimums: " + currentMaximum);
+            Console.ReadKey(); 
+        }
+    }
+}
---
+++ C#/MazeProblem/MinMaxMazeProblemTests/ProgramTests.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MinMaxMazeProblem;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinMaxMazeProblem.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        public class ConsoleOutput : IDisposable
+        {
+            private StringWriter stringWriter;
+            private TextWriter originalOutput;
+
+            public ConsoleOutput()
+            {
+                stringWriter = new StringWriter();
+                originalOutput = Console.Out;
+                Console.SetOut(stringWriter);
+            }
+
+            public string GetOuput()
+            {
+                return stringWriter.ToString();
+            }
+
+            public void Dispose()
+            {
+                Console.SetOut(originalOutput);
+                stringWriter.Dispose();
+            }
+        }
+
+        [TestMethod()]
+        public void testCorrectPathPrint()
+        {
+            var consoleOutput = new ConsoleOutput();
+            string expectedOutput = "7" + "\t" + "11" + "\t" + "1" + "\t" + "12" + "\t" + "10" + "\t" + Environment.NewLine +
+                                    "7" + "\t" + "11" + "\t" + "2" + "\t" + "12" + "\t" + "10" + "\t" + Environment.NewLine +
+                                    "7" + "\t" + "11" + "\t" + "2" + "\t" + "43" + "\t" + "10" + "\t" + Environment.NewLine +
+                                    "7" + "\t" + "9" +  "\t" + "2" + "\t" + "12" + "\t" + "10" + "\t" + Environment.NewLine +
+                                    "7" + "\t" + "9" +  "\t" + "2" + "\t" + "43" + "\t" + "10" + "\t" + Environment.NewLine +
+                                    "7" + "\t" + "9" +  "\t" + "1" + "\t" + "43" + "\t" + "10" + "\t" + Environment.NewLine;
+            List<int> path = new List<int>();
+            int currentMaximum = 0;
+            int x = 0, y = 0;
+            int rowCount = 0, colCount = 0;
+            int[,] m = {   { 7, 11, 1 },
+                           { 9, 2, 12 },
+                           { 1, 43, 10 }};
+
+            rowCount = m.GetLength(0);
+            colCount = m.GetLength(1);
+            Program.possiblePaths(x, y, m, rowCount, colCount, currentMaximum, path);
+            string actualOutput = consoleOutput.GetOuput();
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void testCorrectMaximumReturned()
+        {
+            List<int> path = new List<int>();
+            int currentMaximum = 0;
+            int x = 0, y = 0;
+            int rowCount = 0, colCount = 0;
+            int[,] m = {   { 7, 11, 1 },
+                           { 9, 2, 12 },
+                           { 1, 43, 10 }};
+
+            rowCount = m.GetLength(0);
+            colCount = m.GetLength(1);
+            int actualMaximum = Program.possiblePaths(x, y, m, rowCount, colCount, currentMaximum, path);
+            int expectedMaximum = 2;
+            Assert.AreEqual(expectedMaximum, actualMaximum);
+        }
+
+        [TestMethod()]
+        public void testFailCaseMaximumReturned()
+        {
+            List<int> path = new List<int>();
+            int currentMaximum = 0;
+            int x = 0, y = 0;
+            int rowCount = 0, colCount = 0;
+            int[,] m = {   { 7, 11, 27 },
+                           { 9, 50, 12 },
+                           { 21, 43, 10 }};
+
+            rowCount = m.GetLength(0);
+            colCount = m.GetLength(1);
+            int actualMaximum = Program.possiblePaths(x, y, m, rowCount, colCount, currentMaximum, path);
+            int expectedMaximum = 2;
+            Assert.AreNotEqual(expectedMaximum, actualMaximum);
+        }
+
+        [TestMethod()]
+        public void testNewDataMaximumReturned()
+        {
+            List<int> path = new List<int>();
+            int currentMaximum = 0;
+            int x = 0, y = 0;
+            int rowCount = 0, colCount = 0;
+            int[,] m = {   { 40, 11, 27 },
+                           { 9, 50, 12 },
+                           { 21, 43, 100 }};
+
+            rowCount = m.GetLength(0);
+            colCount = m.GetLength(1);
+            int actualMaximum = Program.possiblePaths(x, y, m, rowCount, colCount, currentMaximum, path);
+            int expectedMaximum = 11;
+            Assert.AreEqual(expectedMaximum, actualMaximum);
+        }
+
+        [TestMethod()]
+        public void testBiggerMatrixMaximumReturned()
+        {
+            List<int> path = new List<int>();
+            int currentMaximum = 0;
+            int x = 0, y = 0;
+            int rowCount = 0, colCount = 0;
+            int[,] m = {   { 40, 110, 27, 31, 100, 2},
+                           { 9, 50, 12, 88, 12, 21 },
+                           { 4, 29, 110, 44, 12, 21 },
+                           { 31, 60, 33, 88, 95, 21 },
+                           { 23, 5, 12, 71, 12, 21 },
+                           { 21, 43, 100, 8, 1, 65 }};
+
+            rowCount = m.GetLength(0);
+            colCount = m.GetLength(1);
+            int actualMaximum = Program.possiblePaths(x, y, m, rowCount, colCount, currentMaximum, path);
+            int expectedMaximum = 21;
+            Assert.AreEqual(expectedMaximum, actualMaximum);
+        }
+
+        [TestMethod()]
+        public void testSmallerMatrixMaximumReturned()
+        {
+            List<int> path = new List<int>();
+            int currentMaximum = 0;
+            int x = 0, y = 0;
+            int rowCount = 0, colCount = 0;
+            int[,] m = {   { 40, 11},
+                           { 15, 43}};
+
+            rowCount = m.GetLength(0);
+            colCount = m.GetLength(1);
+            int actualMaximum = Program.possiblePaths(x, y, m, rowCount, colCount, currentMaximum, path);
+            int expectedMaximum = 15;
+            Assert.AreEqual(expectedMaximum, actualMaximum);
+        }
+    }
+}
